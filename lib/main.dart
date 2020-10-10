b426c3f8134e10b3e13ed67a56f7d6ff1b5ef6eb
DutchDestroyer <wijnbergenmark@gmail.com>
2020-10-10 16:55:31 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    "First commit of this repo on World Mental Health Day, 10-10-2020",
+    textDirection: TextDirection.ltr,
+  )));
+}
