358bc45f0fa731b4042db75a4c3bea435341f46b
DutchDestroyer <wijnbergenmark@gmail.com>
2020-12-13 16:11:03 +0100

    Update models and remove json serializers

--- lib/models/questionnaires/generic_question.dart
+++ lib/models/questionnaires/generic_question.dart
@@ -1,19 +1,9 @@
-import 'package:json_annotation/json_annotation.dart';
-
 import 'equestion_type.dart';
 
-part 'generic_question.g.dart';
-
-@JsonSerializable()
 class GenericQuestion {
   final String question;
   final QuestionType questionType;
   final List<String> answers;
 
   GenericQuestion(this.question, this.questionType, this.answers);
-
-  factory GenericQuestion.fromJson(Map<String, dynamic> json) =>
-      _$GenericQuestionFromJson(json);
-
-  Map<String, dynamic> toJson() => _$GenericQuestionToJson(this);
 }
--- lib/models/questionnaires/generic_questionnaire.dart
+++ lib/models/questionnaires/generic_questionnaire.dart
@@ -1,20 +1,10 @@
-import 'package:json_annotation/json_annotation.dart';
-
 import 'base_questionnaire.dart';
 import 'generic_question.dart';
 
-part 'generic_questionnaire.g.dart';
-
-@JsonSerializable(explicitToJson: true)
 class GenericQuestionnaire extends BaseQuestionnaire {
   final List<GenericQuestion> questions;
 
   GenericQuestionnaire(this.questions, String title, String description,
       bool displayAnswers, String finalRemark)
       : super(title, description, displayAnswers, finalRemark);
-
-  factory GenericQuestionnaire.fromJson(Map<String, dynamic> json) =>
-      _$GenericQuestionnaireFromJson(json);
-
-  Map<String, dynamic> toJson() => _$GenericQuestionnaireToJson(this);
 }
